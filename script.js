0d767b234e85ca641f8e5a8132a9d87b1870939f
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-25 14:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let inputArr = document.querySelectorAll('.code');
+console.log(inputArr);
+
+inputArr.forEach((item) => {
+	item.addEventListener('input', focusNext);
+	item.addEventListener('keydown', focusBack);
+});
+
+function focusNext(){
+	let arr = [...inputArr];
+	let currIndex = arr.indexOf(this);
+
+	if(this.value !== "" && currIndex < arr.length-1){
+		arr[currIndex+1].focus();
+	}
+}
+function focusBack(event){
+	let arr = [...inputArr];
+	let currIndex = arr.indexOf(this);
+
+	if(event.key === "Backspace"){
+		if(this.value==="" && currIndex>0){
+			arr[currIndex-1].focus();
+		}
+	}
+}
